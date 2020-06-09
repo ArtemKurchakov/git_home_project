c1e81611733b62f638bc9d31c6a6f9afa707db1d
Artem <artemkurchakov1018@gmail.com>
2020-06-09 10:15:30 +0300

    artem boxer info added

---
+++ artem_boxer.py
@@ -0,0 +1,11 @@
+class Boxer():
+
+    def __init__(self, age, weight, height):
+        self.age = age
+        self.weight = weight
+        self.height = height
+
+artem = Boxer(34, 100, 187)
+
+
+print(artem.age, artem.weight, artem.height)
